16793c3c291bc010b6e85d66149659ee977a25c9
刘振亚 <liuzhy@df-mic.com>
2021-06-07 20:41:55 +0800

    例行更新

--- src/components/VueDemo.vue
+++ src/components/VueDemo.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <df-x-table :data='[]'></df-x-table>
-  </div>
+  <df-table
+    :data="[]"
+    style="width: 100%"
+    empty-text="这里没有数据了">
+    <df-table-column prop="date" label="日期" width="180" />
+    <df-table-column prop="name" label="姓名" width="180" />
+    <df-table-column prop="address" label="地址" width="1800" />
+  </df-table>
 </template>
-
-<script>
-export default {
-}
-</script>
